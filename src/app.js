3c7af28c15598f1676fbcaea5398f7ee2600f2e4
lycia <lycianex27@gmail.com>
2024-03-31 02:38:37 +0200

    javascript & css update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-alert("hello");
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change the delay to your preference (in milliseconds)
+}
